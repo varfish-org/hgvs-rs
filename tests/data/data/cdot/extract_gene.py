0e57451d223f8eac8fe93dd25ac252c47386d2bf
Manuel Holtgrewe <manuel.holtgrewe@bih-charite.de>
2023-11-08 10:27:28 +0100

    chore: extending extract_gene.py for multiple HGNC IDs (#140)

--- tests/data/data/cdot/extract_gene.py
+++ tests/data/data/cdot/extract_gene.py
@@ -8,7 +8,7 @@ import sys
 import typing
 
 #: HGNC identifier ofs gene to extract, from env or BRCA1.
-HGNC_IDS = os.environ.get("HGNC_ID", "HGNC:672").split(",")
+HGNC_IDS = os.environ.get("HGNC_IDS", "HGNC:1100").split(",")
 
 
 def load_json(json_path: str) -> typing.Dict[str, typing.Any]:
@@ -27,15 +27,18 @@ def extract_gene(
 ) -> typing.Dict[str, typing.Any]:
     """Extract data of one gene, specified by HGNC identifier without prefix."""
     full_data = load_json(json_path)
+    print("gettings keys...", file=sys.stderr)
+    keys = []
+    for key, gene in full_data["genes"].items():
+        if "hgnc" in gene and gene["hgnc"] in hgncs_stripped and key not in keys:
+            keys.append(key)
+    print(f"- keys = {keys}", file=sys.stderr)
     print("extracting...", file=sys.stderr)
-    full_data["genes"] = {key: full_data["genes"][key] for key in hgncs_stripped}
-    gene_symbols = [
-        full_data["genes"][hgnc_id]["gene_symbol"] for hgnc_id in hgncs_stripped
-    ]
+    full_data["genes"] = {key: full_data["genes"][key] for key in keys}
     full_data["transcripts"] = {
         key: value
         for key, value in full_data["transcripts"].items()
-        if value["gene_name"] in gene_symbols
+        if "hgnc" in value and value["hgnc"] in hgncs_stripped
     }
     return full_data
 
@@ -45,10 +48,9 @@ def main(json_paths: typing.List[str]):
     hgncs_stripped = [hgnc_id.replace("HGNC:", "") for hgnc_id in HGNC_IDS]
     for json_path in json_paths:
         gene_data = extract_gene(json_path, hgncs_stripped)
-        symbols = [
-            gene_data["genes"][hgnc_id]["gene_symbol"].lower()
-            for hgnc_id in hgncs_stripped
-        ].join("_")
+        symbols = "_".join(
+            [gene["gene_symbol"].lower() for gene in gene_data["genes"].values()]
+        )
         out_path = json_path.replace(".gz", "").replace(".json", f".{symbols}.json")
         print(f"writing to {out_path}...", file=sys.stderr)
         with open(out_path, "wt") as outputf:
